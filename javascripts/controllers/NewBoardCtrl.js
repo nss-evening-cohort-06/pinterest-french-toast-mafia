b78f0bb5cacf462c5571e97c0185efa43ab38a08
Alex Sanchez <alesanchez728@gmail.com>
2017-11-28 09:23:19 -0600

    added form and DataService to handle db calls

---
+++ javascripts/controllers/NewBoardCtrl.js
@@ -0,0 +1,20 @@
+'use strict';
+
+app.controller("NewBoardCtrl", function ($location, $rootScope, $scope, DataService) {
+
+  $scope.master = {};
+console.log("here!");
+
+  $scope.addNew = ((boardInfo) => {
+    boardInfo.uId = $rootScope.uid;
+    $scope.master = angular.copy(boardInfo);
+    let newBoard =  DataService.createNewBoard($scope.master);
+    console.log("newBoard", newBoard);
+    DataService.postNewBoard(newBoard).then((results) => {
+      $location.path('/boards/myBoards');
+    }).catch((err) => {
+      console.log("error in addNew board", err);
+    }); // end post
+  }); // end addNew
+
+}); // end controller
---
+++ javascripts/services/DataService.js
@@ -0,0 +1,25 @@
+'use strict';
+
+app.service("DataService", function ($http, $scope, FIREBASE_CONFIG) {
+
+
+  const postNewBoard = (newBoard) => {
+    return $http.post(`${FIREBASE_CONFIG.databaseURL}/boards.json`, JSON.stringify(newBoard));
+  };
+
+  const createBoard = (boardInfo) => {
+    return {
+      "name": boardInfo.name,
+      "description": boardInfo.description,
+      "is_secret": boardInfo.isSecret,
+      "id": boardInfo.id,
+      "uId": boardInfo.uId,
+    };
+  };
+
+  return {
+    postNewBoard,
+    createBoard
+  };
+  
+});
