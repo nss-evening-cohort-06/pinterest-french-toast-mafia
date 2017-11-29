902972f8971b43841c2cecf1a3f9df6cf5fe2994
ben harrington <harringtonben@icloud.com>
2017-11-28 20:22:35 -0600

    fixed console log

--- javascripts/controllers/MyBoardsCtrl.js
+++ javascripts/controllers/MyBoardsCtrl.js
@@ -4,7 +4,6 @@ app.controller("MyBoardsCtrl", function($rootScope, $scope, DataService) {
 
     const getDaBoards = () => {
         DataService.getMyBoards($rootScope.uid).then((results) => {
-            console.log(results);
             $scope.boards = results;
        }).catch((error) => {
             console.log("Error in getMyBoards", error);
