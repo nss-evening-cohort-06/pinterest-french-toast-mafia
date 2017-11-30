ae6bfaa7ac7794e4e81475bbddd8c573069b6bfe
Alex Sanchez <alesanchez728@gmail.com>
2017-11-30 09:50:47 -0600

    profile

--- javascripts/controllers/MyBoardsCtrl.js
+++ javascripts/controllers/MyBoardsCtrl.js
@@ -17,19 +17,32 @@ app.controller("MyBoardsCtrl", function ($location, $rootScope, $scope, DataServ
 
 
     $scope.deleteMyBoard = (Id) => {
-    DataService.deleteBoard(Id).then((result) =>{
-      getDaBoards();
-    }).catch((err) =>{
-      console.log("error in deleteMyBoard", err);
-    });
+        DataService.deleteBoard(Id).then((result) => {
+            getDaBoards();
+        }).catch((err) => {
+            console.log("error in deleteMyBoard", err);
+        });
 
 
-  };
+    };
 
-  $scope.editMyBoard = (boardId) => {
-    $location.path(`/board/${boardId}`);
-};
+    $scope.editMyBoard = (boardId) => {
+        $location.path(`/board/${boardId}`);
+    };
 
+    $scope.seeMyPins = () => {
+        $location.path(`/mypins`);
+    };
+
+    const getUserName = () => {
+        DataService.getUserInfo($rootScope.uid).then((results) => {
+            $scope.user = results[0];
+        }).catch((err) => {
+            console.log("error in getUserName", err);
+        });
+    };
+
+    getUserName();
 
 
 });
--- javascripts/controllers/NavCtrl.js
+++ javascripts/controllers/NavCtrl.js
@@ -2,13 +2,6 @@
 
 app.controller("NavCtrl", function ($location, $rootScope, $scope, $window, AuthService) {
 
-// $scope.user = {};
-
-//   const getUserName = () => {
-
-//   };
-
-
   $scope.logoutUser = () => {
     delete $rootScope.uid;
     $window.localStorage.clear();
